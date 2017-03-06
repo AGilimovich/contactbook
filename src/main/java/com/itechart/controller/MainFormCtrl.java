039fe943b6441ed7414174dd245a90c9dafc1e4c
AGilimovich <a.gilimovich1990@gmail.com>
2017-03-06 16:58:47 +0300

    initial commit

---
+++ src/main/java/com/itechart/controller/MainFormCtrl.java
@@ -0,0 +1,7 @@
+package com.itechart.controller;
+
+/**
+ * This class is a controller of all requests to the server.
+ */
+public class MainFormCtrl extends  {
+}
---
+++ src/main/java/com/itechart/entity/Address.java
@@ -0,0 +1,13 @@
+package com.itechart.entity;
+
+/**
+ * Represents living address of contact.
+ */
+public class Address {
+    private String country;
+    private String city;
+    private String Street;
+    private int house;
+    private int apartment;
+    private int zipCode;
+}
